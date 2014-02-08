205592aa1019e42cea166e8789bd31883c460db1
KaotoTempest <uk_alien@hotmail.com>
2014-02-08 15:25:39 +0000

    Update poly & vector

--- CrazyGolf/src/Main.java
+++ CrazyGolf/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Adam Bedford on 08/02/14.
@@ -9,6 +11,43 @@ public class Main {
 
     public static void main(String args[]){
 
+        frame = new Frame("Crazy Golf");
+        frame.setSize(1000, 600);
 
+        Panel toolbarPanel = new Panel();
+        toolbarPanel.setPreferredSize(new Dimension(1000, 30));
+        toolbarPanel.setBackground(Color.black);
+        toolbarPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        Panel appletPanel = new Panel();
+        appletPanel.setPreferredSize(new Dimension(1000, 540));
+        appletPanel.setBackground(Color.yellow);
+
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.add(toolbarPanel);
+        frame.add(appletPanel);
+
+        Button drawPoly = new Button("DrawPoly");
+        toolbarPanel.add(drawPoly);
+        drawPoly.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        Button drawSquare = new Button("DrawSquare");
+        toolbarPanel.add(drawSquare);
+        Button drawTriangle = new Button("DrawTriangle");
+        toolbarPanel.add(drawTriangle);
+        Button setStartPoint = new Button("SetStart");
+        toolbarPanel.add(setStartPoint);
+        Button setHole = new Button("SetHole");
+        toolbarPanel.add(setHole);
+
+        app = new applet();
+        appletPanel.add(app);
+
+        frame.setVisible(true);
     }
 }
--- CrazyGolf/src/applet.java
+++ CrazyGolf/src/applet.java
@@ -1,5 +1,6 @@
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Adam Bedford on 08/02/14.
@@ -8,12 +9,14 @@ public class applet extends Applet {
     Core core;
 
     @Override
-    public void update(Graphics g){
-
+    public void update(Graphics g) {
+        Image image = createImage(getWidth(), getHeight());
+        paint(image.getGraphics());
+        g.drawImage(image, 0, 0, this);
     }
 
     @Override
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
 
     }
 }
