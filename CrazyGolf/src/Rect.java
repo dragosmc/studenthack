d69d9dd41fdac3945e25d46456edd45a84a53b23
PringleLiam <pringle.liam@yahoo.co.uk>
2014-02-08 14:14:42 +0000

    bananarama

--- CrazyGolf/src/Rect.java
+++ CrazyGolf/src/Rect.java
@@ -5,11 +5,18 @@ import java.util.List;
  * Created by Adam Bedford on 08/02/14.
  */
 public class Rect implements Primitive {
-    public Point centre;
-    public Point corner;
+    private Point centre;
+    private Point corner;
 
     @Override
-    public List<Collisionable> resolve() {
-        return null;
+    public void resolve(List<Collisionable> list) {
+    }
+
+    public Point getCentre(){
+        return centre;
+    }
+
+    public Point getCorner(){
+        return corner;
     }
 }
--- CrazyGolf/src/Triangle.java
+++ CrazyGolf/src/Triangle.java
@@ -5,11 +5,10 @@ import java.util.List;
  * Created by Adam Bedford on 08/02/14.
  */
 public class Triangle implements Primitive {
-    public Point centre;
-    public Point corner;
+    private Point centre;
+    private Point corner;
 
     @Override
-    public List<Collisionable> resolve() {
-        return null;
+    public void resolve(List<Collisionable> list) {
     }
 }
